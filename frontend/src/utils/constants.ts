f0d109e5c3265f55d9fbd4661ef4779dcb3267ed
Danilo Araújo <daniloaraujo1990@hotmail.com>
2024-12-11 16:50:46 -0300

    Constante para endereços das rotas

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -3,6 +3,10 @@ export enum RoutesPath {
   LOGIN = "/login",
   CATEGORIAS = "/categorias",
   EDITAR_CATEGORIAS = "/categorias/:id",
+  REGISTRAR_CATEGORIAS = "/categorias/registrar",
   PRODUTOS = "/produtos",
   EDITAR_PRODUTOS = "/produtos/:id",
+  REGISTRAR_PRODUTOS = "/produtos/registrar",
 }
+
+export const formatIdRoute = (route:string, id:number):string => route.replace(":id",id.toString())
