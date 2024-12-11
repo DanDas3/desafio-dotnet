a8b06823a8441ac966a391b468e89b4349ba2490
Danilo Araújo <daniloaraujo1990@hotmail.com>
2024-12-11 16:38:33 -0300

    Atualização do provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,27 @@ import './App.css'
 import AppRoutes from './routes/AppRoutes'
 import { Provider } from 'react-redux'
 import store from './store'
+import AppMenu from './components/AppMenu/AppMenu'
+import { BrowserRouter } from 'react-router-dom'
+import { UtilsProvider } from './context/utilsContext'
+import { AuthProvider } from './context/authContext'
+import Private from './private/private'
 
 function App() {
-
   return (
-    <Provider store={store}>
-      <AppRoutes />
-      
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <UtilsProvider>
+          <AuthProvider>
+            <Private>
+              <AppMenu>
+                <AppRoutes />
+              </AppMenu>
+            </Private>
+          </AuthProvider>
+        </UtilsProvider>
+      </Provider>
+    </BrowserRouter>
   )
 }
 
